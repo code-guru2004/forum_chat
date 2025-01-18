89a32994a48a60f9d0caecd23cc6ea4069cf5614
Nayan Das <nayan010das@gmail.com>
2025-01-18 13:33:42 +0530

    POST request added

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -5,12 +5,16 @@ const api_key = "h8n6umc8an99";
 const api_secret = 'fxjkd5c4k5fc5dqnr23q3t4q83pwz2gnrbgughbs2am34t53hvb54zm6exwnnp3h';
 const user_id = "user_2rkzjuQB9KJIyM96XYEK4IEM9tb";
 
-export async function GET() {
+export async function POST({request}) {
   // Initialize a Server Client
   const serverClient = StreamChat.getInstance(api_key, api_secret);
   // Create User Token
-  const token = serverClient.createToken(user_id);
-    console.log(token);
+  // const token = serverClient.createToken(user_id);
+  //   console.log(token);
+  console.log("fdgfg");
+  
+    const body = await request.body;
+    console.log(body);
     
   return Response.json({msg:"Hello"})
 }
